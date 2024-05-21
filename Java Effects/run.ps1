16381ce98bfecd48f4a7e1090f098acff97968ec
jessifedalto <142901908+jessifedalto@users.noreply.github.com>
2024-05-21 16:02:24 -0300

    Create run.ps1

---
+++ Java Effects/run.ps1
@@ -0,0 +1,12 @@
+if ((ls | ? {$_.Name -eq "bin" }).Count -eq 0) {
+    mkdir bin
+}
+
+javac App.java -d bin
+ls | ? {$_.Name.EndsWith(".fxml") } | % { cp $_ bin}
+
+clear
+javac App.java -d bin
+cd bin
+java App
+cd ..
